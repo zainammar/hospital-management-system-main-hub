07d5f3f692d5fa17409bc7fb58029da8a61993ef
zainammar <zainammar74@gmail.com>
2026-06-19 17:59:00 +0500

    Add Fee

--- patient/models.py
+++ patient/models.py
@@ -49,6 +49,13 @@ class Patient(models.Model):
         null=True
     )
 
+    # Patient Fee
+    fee = models.DecimalField(
+        max_digits=10,
+        decimal_places=2,
+        default=0.00
+    )
+
     status = models.CharField(
         max_length=20,
         choices=STATUS_CHOICES,
@@ -60,6 +67,3 @@ class Patient(models.Model):
 
     def __str__(self):
         return self.patient_name
-    
-
-    
